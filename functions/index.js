0ed69cce8ffc96ed4d54896b23740b0436610bd2
Jonathan Chiam <jczjchiam@gmail.com>
2017-10-08 23:22:05 +0800

    Setup cloud function to get pokemon details by id

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,10 @@ const cors = require('cors')({ origin: true });
 const CLOUDINARY_NAME = functions.config().cloudinary.name;
 const CLOUDINARY_KEY = functions.config().cloudinary.key.substring(1);
 const CLOUDINARY_SECRET = functions.config().cloudinary.secret;
-
 const CLOUDINARY_BASE_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_NAME}`;
-
 const CLOUDINARY_AUTH = { username: CLOUDINARY_KEY, password: CLOUDINARY_SECRET };
+
+const POKEAPI_BASE_URL = 'https://pokeapi.co/api/v2';
 
 exports.gridThumbnails = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
@@ -21,5 +21,17 @@ exports.gridThumbnails = functions.https.onRequest((req, res) => {
     )
       .then(response => response.data)
       .then(data => res.status(200).send(data.resources));
+  });
+});
+
+exports.getPokemonById = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    const id = req.query.id;
+    if (!id) {
+      res.status(404).send();
+    }
+    axios.get(`${POKEAPI_BASE_URL}/pokemon/${id}`)
+      .then(response => response.data)
+      .then(data => res.status(200).send(data));
   });
 });
